6b8677fd183f65efc6f70fea364fdf53b123a481
Britefury <Britefury@localhost>
2013-11-05 16:03:29 +0000

    Removed size parameter from text_entry control, fixed width parameter. Added docstring to select control. Wrote documentation for text_entry control.

--- larch/controls/select.py
+++ larch/controls/select.py
@@ -8,8 +8,18 @@ from larch.pres.pres import CompositePres
 
 class select (CompositePres):
 	def __init__(self, option_value_content_pairs, value, on_choose=None):
+		"""
+		HTML select control
+
+		:param option_value_content_pairs: a sequence of tuple-pairs describing the options. Each pair is of the form (value, text)
+		:param value: the initial value
+		:param on_choose: a callback function of the form fn(event, value) that is invoked when the user makes a choice
+		"""
 		self.__options = [Html('<option value="{0}"{1}>'.format(opt_value, (' selected'   if opt_value == value   else '')), content, '</option>')   for opt_value, content in option_value_content_pairs]
 		self.choose = EventHandler()
+		if on_choose is not None:
+			self.choose.connect(on_choose)
+
 
 	def pres(self, pres_ctx):
 		p = Html(*(['<select>'] + self.__options + ['</select>'])).js_function_call('larch.controls.initSelect')
--- larch/controls/text_entry.py
+++ larch/controls/text_entry.py
@@ -1,6 +1,7 @@
 ##-*************************
 ##-* This source code is (C)copyright Geoffrey French 2011-2013.
 ##-*************************
+from larch.event_handler import EventHandler
 from larch.pres.html import Html
 from larch.pres.pres import CompositePres
 from larch.pres.resource import MessageChannel
@@ -9,21 +10,26 @@ from larch.pres.resource import MessageChannel
 
 
 class text_entry (CompositePres):
-	def __init__(self, text, immediate_events=False, on_edit=None, size=None, width=None):
+	def __init__(self, text, immediate_events=False, on_edit=None, width=None):
 		"""
 		Create a text entry control
 		:param text: the initial text to display in the control
 		:param immediate_events: if True, an event will be emitted each time the text is edited (on each keypress)
 		:param on_edit: a callback invoked in response to edits, of the form function(modified_text)
-		:param size: size of control in characters
-		:param width: width, controlled by CSS
+		:param width: width of the control; ints or longs will be interpreted as width in pixels, otherwise use string in CSS form, e.g. '100px', '10em' or '50%'
 		:return: the editor control
 		"""
+		self.edit = EventHandler()
+
+		if isinstance(width, int)  or  isinstance(width, long):
+			width = '{0}px'.format(width)
+
 		self.__text = text
 		self.__immediate_events = immediate_events
-		self.__on_edit = on_edit
-		self.__size = size
 		self.__width = width
+
+		if on_edit is not None:
+			self.edit.connect(on_edit)
 
 		self.__channel = MessageChannel()
 
@@ -32,19 +38,12 @@ class text_entry (CompositePres):
 		self.__channel.send(text)
 
 
-	def __edit(self, event):
-		if self.__on_edit is not None:
-			self.__on_edit(event, event.data)
-
-
 	def pres(self, pres_ctx):
 		sz = ''
-		if self.__size is not None:
-			sz += ' size="{0}"'.format(self.__size)
 		if self.__width is not None:
-			sz += ' style="width: {0};"'.format(self.__width)
+			sz = ' style="width: {0};"'.format(self.__width)
 		p = Html('<input type="text" value="{0}"{1}></input>'.format(self.__text, sz)).js_function_call('larch.controls.initTextEntry', self.__immediate_events, self.__channel)
-		p = p.with_event_handler('text_entry_edit', self.__edit)
+		p = p.with_event_handler('text_entry_edit', lambda event: self.edit(event, event.data))
 		p = p.use_js('/static/larch/larch_ui.js').use_css('/static/larch/larch_ui.css')
 		return p
 
@@ -52,19 +51,17 @@ class text_entry (CompositePres):
 
 
 class live_text_entry (CompositePres):
-	def __init__(self, live, immediate_events=False, size=None, width=None):
+	def __init__(self, live, immediate_events=False, width=None):
 		"""
 		Text entry control that edits a live value
 
 		:param live: live value object whose value is to be edited
 		:param immediate_events: if True, an event will be emitted each time the text is edited (on each keypress)
-		:param size: size of control in characters
 		:param width: width, controlled by CSS
 		:return: the editor control
 		"""
 		self.__live = live
 		self.__immediate_events = immediate_events
-		self.__size = size
 		self.__width = width
 
 
@@ -85,6 +82,7 @@ class live_text_entry (CompositePres):
 
 		self.__live.add_listener(on_change)
 
-		s = text_entry(self.__live.static_value, self.__immediate_events, __on_edit, self.__size, self.__width)
+		s = text_entry(self.__live.static_value, self.__immediate_events, width=self.__width)
+		s.edit.connect(__on_edit)
 		return s
 
