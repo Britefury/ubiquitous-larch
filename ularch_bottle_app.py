e311da4638374e5b14ea8ed72dc3896600ea24d0
Britefury <Britefury@localhost>
2014-01-05 15:59:08 +0000

    Added global password authentication to bottle app.

--- ularch_bottle_app.py
+++ ularch_bottle_app.py
@@ -3,7 +3,7 @@
 ##-*************************
 import webbrowser
 
-import sys
+import sys, os, datetime
 
 from bottle import Bottle, static_file, request, response, redirect
 
@@ -14,11 +14,182 @@ from larch.apps import larch_app
 from larch.hub import larch_hub
 
 
-def make_ularch_bottle_app(docpath=None, documentation_path=None):
-	hub = larch_hub.start_hub_and_client('main', 'larchapp', larch_app.create_service, '/main/larchapp', docpath, documentation_path=documentation_path)
-
-
+def make_ularch_bottle_app(docpath=None, documentation_path=None, password=None, session_time_hours=24):
+	# Bottle app
 	app = Bottle()
+
+	#
+	# Authentication
+	#
+
+	if password is not None  and  password != '':
+		login_form_page = """
+<html>
+<head>
+<title>Login</title>
+<link rel="stylesheet" type="text/css" href="/static/jquery/css/ui-lightness/jquery-ui-1.10.2.custom.min.css"/>
+<link rel="stylesheet" type="text/css" href="/static/larch/larch.css"/>
+<link rel="stylesheet" type="text/css" href="/static/larch/larch_login.css"/>
+<script type="text/javascript" src="/static/jquery/js/jquery-1.9.1.js"></script>
+<script type="text/javascript" src="/static/jquery/js/jquery-ui-1.10.2.custom.min.js"></script>
+</head>
+
+
+<body>
+<div class="title_bar">The Ubiquitous Larch</div>
+
+<div class="login_form">
+<p>Please login:</p>
+
+<form action="/accounts/process_login" method="POST">
+<table>
+	<tr><td>Password</td><td><input type="password" name="password" class="login_form_text_field"/></td></tr>
+	<tr><td></td><td><input id="submit_button" type="submit" value="Login"/></td></tr>
+</table>
+</form>
+
+{0}
+
+</div>
+
+<script type="text/javascript">
+	$("#submit_button").button();
+</script>
+</body>
+</html>
+"""
+
+
+		#
+		# VERY BASIC SESSION SYSTEM
+		#
+
+		class Session (object):
+			def __init__(self):
+				self.expiry = datetime.datetime.now() + datetime.timedelta(hours=session_time_hours)
+
+				self.attrs = {}
+
+
+			def __getitem__(self, item):
+				return self.attrs[item]
+
+			def __setitem__(self, key, value):
+				self.attrs[key] = value
+
+			def __delitem__(self, key):
+				del self.attrs[key]
+
+			def get(self, key, default=None):
+				return self.attrs.get(key, default)
+
+
+		app_secret = os.urandom(32)
+
+		sessions = {}
+
+		def get_session():
+			session_key = request.get_cookie('ularch_session_key', secret=app_secret)
+			if session_key is not None:
+				session = sessions.get(session_key)
+				if session is not None:
+					if datetime.datetime.now() < session.expiry:
+						return session
+
+			# New session
+			session_key = os.urandom(16)
+			session = Session()
+			sessions[session_key] = session
+			response.set_cookie('ularch_session_key', session_key, secret=app_secret, max_age=session_time_hours*3600, path='/')
+			return session
+
+
+
+		#
+		# Authentication functions
+		#
+
+		def is_authenticated():
+			session = get_session()
+			return session.get('authenticated', False)
+
+		def authenticate(pwd_from_user):
+			session = get_session()
+			if pwd_from_user == password:
+				session['authenticated'] = True
+				return True
+			else:
+				return False
+
+		def deauthenticate():
+			session = get_session()
+			if session.get('authenticated') is not None:
+				del session['authenticated']
+
+
+		#
+		# Login URLs
+		#
+
+		@app.route('/accounts/login')
+		def login_form():
+			if is_authenticated():
+				return redirect('/pages')
+			else:
+				return login_form_page.format('')
+
+		@app.route('/accounts/process_login', method='POST')
+		def process_login():
+			pwd = request.forms['password']
+
+			if authenticate(pwd):
+				session = get_session()
+				next_path = session.get('next_path')
+				if next_path is None:
+					next_path = '/pages'
+				return redirect(next_path)
+			else:
+				return login_form_page.format('<p>Incorrect password; please try again.</p>')
+
+		@app.route('/accounts/logout')
+		def logout():
+			deauthenticate()
+			return redirect('/')
+
+
+
+		def login_required(fn):
+			def chech_logged_in(*args, **kwargs):
+				if is_authenticated():
+					return fn(*args, **kwargs)
+				else:
+					path = request.path
+					session = get_session()
+					session['next_path'] = path
+					return redirect('/accounts/login')
+			chech_logged_in.__name__ = fn.__name__
+			return chech_logged_in
+
+		logout_url_path = '/accounts/logout'
+	else:
+		# No authentication
+		def login_required(f):
+			return f
+
+		logout_url_path = None
+
+
+
+
+
+	#
+	# Ubiquitous Larch URLs
+	#
+
+
+	hub = larch_hub.start_hub_and_client('main', 'larchapp', larch_app.create_service, '/main/larchapp', docpath, documentation_path=documentation_path, logout_url_path=logout_url_path)
+
+
 
 
 
@@ -28,6 +199,7 @@ def make_ularch_bottle_app(docpath=None, documentation_path=None):
 
 
 	@app.route('/')
+	@login_required
 	def index():
 		redirect('/pages/main/larchapp')
 
@@ -36,6 +208,7 @@ def make_ularch_bottle_app(docpath=None, documentation_path=None):
 	@app.route('/pages/')
 	@app.route('/pages/<category>/<name>')
 	@app.route('/pages/<category>/<name>/')
+	@login_required
 	def root_page(category, name):
 		try:
 			get_params = {}
@@ -47,6 +220,7 @@ def make_ularch_bottle_app(docpath=None, documentation_path=None):
 
 
 	@app.route('/pages/<category>/<name>/<location:path>')
+	@login_required
 	def page(category, name, location):
 		try:
 			get_params = {}
@@ -58,6 +232,7 @@ def make_ularch_bottle_app(docpath=None, documentation_path=None):
 
 
 	@app.route('/event/<category>/<name>/<view_id>', method='POST')
+	@login_required
 	def event(category, name, view_id):
 		event_data = request.forms.get('event_data')
 		data = hub.event(category, name, view_id, event_data)
@@ -66,6 +241,7 @@ def make_ularch_bottle_app(docpath=None, documentation_path=None):
 
 
 	@app.route('/form/<category>/<name>/<view_id>', method='POST')
+	@login_required
 	def form(category, name, view_id):
 		form_data = {}
 
@@ -83,6 +259,7 @@ def make_ularch_bottle_app(docpath=None, documentation_path=None):
 
 
 	@app.route('/rsc/<category>/<name>/<view_id>/<rsc_id>', method='GET')
+	@login_required
 	def rsc(category, name, view_id, rsc_id):
 		data_and_mime_type = hub.resource(category, name, view_id, rsc_id)
 		if data_and_mime_type is not None:
@@ -92,6 +269,9 @@ def make_ularch_bottle_app(docpath=None, documentation_path=None):
 		else:
 			response.status=404
 			return 'Resource not found'
+
+
+
 
 
 	return app
